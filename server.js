722b08e095e7a7e130f70ee2fdbf0409a594511d
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-04-02 17:31:50 +0000

    Node.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = 3000;
+
+// Servindo arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para servir o index.html automaticamente
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
